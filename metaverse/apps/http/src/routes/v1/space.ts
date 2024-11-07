eeb6cfababbdf5baa3f38cc52b0c1171acd7a2b3
Satyam <ysatyam9627@gmail.com>
2024-11-07 23:21:07 +0530

    added metaverse folder and db schema (prisma)

---
+++ metaverse/apps/http/src/routes/v1/space.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+
+export const spaceRouter = Router();
+
+spaceRouter.post("/",(req,res)=>{
+
+})
+
+spaceRouter.delete("/:spaceId",(req,res)=>{
+
+})
+
+spaceRouter.get("/all",(req,res)=>{
+
+})
+
+spaceRouter.get("/:spaceId",(req,res)=>{
+
+})
+
+spaceRouter.get("/element",(req,res)=>{
+
+})
+
+spaceRouter.delete("/element",(req,res)=>{
+
+})
+
+spaceRouter.get("/:spaceId",(req,res)=>{
+
+})
+
+
+
---
+++ metaverse/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { router } from ".";
+
+
+
+export const userRouter = Router();
+
+router.post("/metadata",(req,res)=>{
+
+})
+
+router.get("/metadata/bulk",(req,res)=>{
+
+})
+
+
